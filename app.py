0a215f2b47386c6e8d176ba22bd04afddf26c492
Hundredvisionsguy <winikkc@hsd.k12.or.us>
2024-03-18 15:29:17 -0700

    adding app file

---
+++ app.py
@@ -0,0 +1,21 @@
+"""
+Cad Lib
+By Chris Winikka
+A Mad-lib-like program school project."""
+
+# import statements
+
+
+# Welcome the user
+
+
+# Get user input
+
+
+# Build narrative
+
+
+# Display results
+
+
+# Thank the user and quit
